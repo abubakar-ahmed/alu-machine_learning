b96217760cd3e0bacef9983b6a8d8264602c1e15
abubakar.ahmed <a.ahmed1@alustudent.com>
2024-10-05 04:29:21 -0700

    fixeed bugs again

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,20 +1,6 @@
 #!/usr/bin/env python3
 '''
         Calculate the derivative of a polynomial.
-
-    Args:
-    poly (list): A list of coefficients representing a polynomial, where the index of each element
-                 represents the power of x for that term. For example, if poly = [5, 3, 0, 1],
-                 this represents the polynomial f(x) = x^3 + 3x + 5.
-
-    Returns:
-    list: A list of coefficients representing the derivative of the polynomial.
-          If the derivative is 0, the function returns [0].
-          If the input is invalid (not a list or contains non-numeric values), return None.
-
-    Example:
-    poly = [5, 3, 0, 1]  # Represents f(x) = x^3 + 3x + 5
-    poly_derivative(poly)  # Returns [3, 0, 3] which represents f'(x) = 3x^2 + 3
 '''
 
 def poly_derivative(poly):
