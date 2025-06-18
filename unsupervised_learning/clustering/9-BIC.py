1a29580e9dab69b8d8594c78c3c1abe2cbcb6775
Abubakar Ahmed <128506001+abubakar-ahmed@users.noreply.github.com>
2025-06-18 12:28:31 +0200

    bug fix

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python3
-"""This module contains a function that perfoms
-finds the best number of clusters for a GMM using the
-Bayesian Information Criterion"""
+"""This module contains a function that finds the best number of clusters for a GMM using the Bayesian Information Criterion"""
 import numpy as np
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """
-    finds the best number of clusters for a GMM using the
-    Bayesian Information Criterion
+    Finds the best number of clusters for a GMM using the Bayesian Information Criterion
     """
+    # Input validation
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None
 
     if not isinstance(kmin, int) or kmin < 1:
         return None, None, None, None
 
-    if not isinstance(kmax, int) or kmax < kmin:
+    if kmax is not None and (not isinstance(kmax, int) or kmax < kmin):
         return None, None, None, None
 
-    if not isinstance(iterations, int):
+    if not isinstance(iterations, int) or iterations <= 0:
         return None, None, None, None
 
     if not isinstance(tol, float) or tol < 0:
@@ -29,24 +27,30 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     if not isinstance(verbose, bool):
         return None, None, None, None
 
+    n, d = X.shape
     if kmax is None:
-        kmax = iterations
+        kmax = n  # or set to a reasonable upper limit like 10
 
-    n = X.shape[0]
-    prior_bic = 0
-    likelyhoods = bics = []
-    best_k = kmax
-    pi_prev = m_prev = S_prev = best_res = None
+    best_k = None
+    best_result = None
+    log_likelihoods = []
+    bics = []
+
     for k in range(kmin, kmax + 1):
-        pi, m, S, g, ll = expectation_maximization(X, k, iterations, tol,
-                                                   verbose)
-        bic = k * np.log(n) - 2 * ll
-        if np.isclose(bic, prior_bic) and best_k >= k:
-            best_k = k - 1
-            best_res = pi_prev, m_prev, S_prev
-        pi_prev, m_prev, S_prev = pi, m, S
-        likelyhoods.append(ll)
-        bics.append(bic)
-        prior_bic = bic
+        pi, m, S, g, log_likelihood = expectation_maximization(X, k, iterations, tol, verbose)
 
-    return best_k, best_res, np.asarray(likelyhoods), np.asarray(bics)
+        # Number of parameters in GMM:
+        # p = (k - 1) + k*d + k*d*(d+1)/2
+        cov_params = (d * (d + 1)) / 2
+        p = (k - 1) + k * d + k * cov_params
+
+        bic = p * np.log(n) - 2 * log_likelihood
+
+        log_likelihoods.append(log_likelihood)
+        bics.append(bic)
+
+        if best_k is None or bic < bics[best_k - kmin]:
+            best_k = k
+            best_result = (pi, m, S)
+
+    return best_k, best_result, np.array(log_likelihoods), np.array(bics)
