9f69aa301b63015b75de0046a33ae8b8bc6f916c
Abubakar Ahmed <128506001+abubakar-ahmed@users.noreply.github.com>
2025-06-18 12:41:17 +0200

    last bug

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,21 +1,40 @@
 #!/usr/bin/env python3
-"""This module contains a function that finds the best number of clusters for a GMM using the Bayesian Information Criterion"""
+"""
+This module contains a function that finds the best number of clusters
+for a GMM using the Bayesian Information Criterion
+"""
 import numpy as np
+
 expectation_maximization = __import__('8-EM').expectation_maximization
 
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     """
-    Finds the best number of clusters for a GMM using the Bayesian Information Criterion
+    Finds the best number of clusters for a GMM using BIC
+
+    Parameters:
+    - X: np.ndarray of shape (n, d)
+    - kmin: minimum number of clusters to check (inclusive)
+    - kmax: maximum number of clusters to check (inclusive)
+    - iterations: max iterations for EM
+    - tol: tolerance for EM
+    - verbose: print EM log info
+
+    Returns:
+    - best_k: optimal number of clusters
+    - best_result: tuple (pi, m, S)
+    - l: array of log likelihoods
+    - b: array of BIC values
     """
-    # Input validation
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None, None
 
     if not isinstance(kmin, int) or kmin < 1:
         return None, None, None, None
 
-    if kmax is not None and (not isinstance(kmax, int) or kmax < kmin):
+    if kmax is not None and (
+        not isinstance(kmax, int) or kmax < kmin
+    ):
         return None, None, None, None
 
     if not isinstance(iterations, int) or iterations <= 0:
@@ -29,7 +48,7 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
 
     n, d = X.shape
     if kmax is None:
-        kmax = n  # or set to a reasonable upper limit like 10
+        kmax = n  # or set a reasonable upper limit
 
     best_k = None
     best_result = None
@@ -37,14 +56,18 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     bics = []
 
     for k in range(kmin, kmax + 1):
-        pi, m, S, g, log_likelihood = expectation_maximization(X, k, iterations, tol, verbose)
+        try:
+            pi, m, S, g, log_likelihood = expectation_maximization(
+                X, k, iterations, tol, verbose
+            )
+            if log_likelihood is None:
+                continue
+        except Exception:
+            continue
 
-        # Number of parameters in GMM:
-        # p = (k - 1) + k*d + k*d*(d+1)/2
         cov_params = (d * (d + 1)) / 2
         p = (k - 1) + k * d + k * cov_params
-
-        bic = p * np.log(n) - 2 * log_likelihood
+        bic = (p * np.log(n)) - (2 * log_likelihood)
 
         log_likelihoods.append(log_likelihood)
         bics.append(bic)
@@ -53,4 +76,10 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
             best_k = k
             best_result = (pi, m, S)
 
-    return best_k, best_result, np.array(log_likelihoods), np.array(bics)
+    if best_k is None:
+        return None, None, None, None
+
+    return (best_k,
+            best_result,
+            np.array(log_likelihoods),
+            np.array(bics))
